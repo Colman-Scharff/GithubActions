a6f2bb71bfb612a0fde03f3e8fc2efc05a91b2af
Colman-Scharff <111661630+Colman-Scharff@users.noreply.github.com>
2024-04-12 22:34:35 -0500

    Update UnitTests.cs
    added unit tests for the 4 other operations

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,6 +30,102 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        
+        [Test]
+        public void Subtract_Valid_Scharff()
+        {
+            Assert.AreEqual(-1, Program.Subtract("1", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(0, Program.Subtract("3", "3"));
+        }
+
+        [Test]
+        public void Subtract_Invalid_Scharff()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null_Scharff()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_Valid_Scharff()
+        {
+            Assert.AreEqual(2, Program.Multiply("1", "2"));
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(35, Program.Multiply("5", "7"));
+        }
+
+        [Test]
+        public void Multiply_Invalid_Scharff()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_Scharff()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
+        [Test]
+        public void Divide_Valid_Scharff()
+        {
+            Assert.AreEqual(0.5, Program.Add("1", "2"));
+            Assert.AreEqual(3, Program.Add("6", "2"));
+            Assert.AreEqual(5, Program.Add("25", "5"));
+        }
+
+        [Test]
+        public void Divide_Invalid_Scharff()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null_Scharff()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
+        [Test]
+        public void Power_Valid_Scharff()
+        {
+            Assert.AreEqual(3, Program.Add("1", "2"));
+            Assert.AreEqual(9, Program.Add("3", "2"));
+            Assert.AreEqual(32, Program.Add("2", "5"));
+        }
+
+        [Test]
+        public void Power_Invalid_Scharff()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Power_Null_Scharff()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
     }
 }
